123cb39422f25d93b5ea8ac59783e233ef27c0fb
Krzysztof Kotlarski <kkotlarski@gmail.com>
2014-04-01 08:40:04 +0200

    Observer

---
+++ code/js/Observer.js
@@ -0,0 +1,34 @@
+var APP = APP || {};
+
+APP.Observer = {
+    _events: {},
+
+    on: function (event, fn, scope) {
+        if (typeof this._events[event] === 'undefined') {
+            this._events[event] = [];
+        }
+        this._events[event].push({fn: fn, scope: scope || window);
+    },
+
+    off: function (event, fn, scope) {
+        if (typeof this._events[event] !== 'undefined') {
+            var n = this._events[event].length;
+            while (n--) {
+                if (this._events[event][n].fn === fn && (typeof scope === 'undefined' || scope === this._events[event][n].scope)) {
+                    this._events[event].splice(n, 1);
+                }
+            }
+
+        }
+    },
+
+    emit: function (event, data) {
+        if (typeof this._events[event] !== 'undefined') {
+            var i = 0,
+                n = this._events[event].length;
+            for (i; i < n; ++i) {
+                this._events[event][i].fn.call(this._events[event][i].scope, data);
+            }
+        }
+    }
+};
