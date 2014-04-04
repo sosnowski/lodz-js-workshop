8f208e85cba488f1794b76cc6e02d3cc4447d3e0
Krzysztof Kotlarski <kkotlarski@gmail.com>
2014-04-04 19:00:19 +0200

    persistent db

--- server/server.js
+++ server/server.js
@@ -1,36 +1,52 @@
-var io = require('socket.io').listen(8080),
-	_tweets = [],
-	_users = {};
+var io = require('socket.io').listen(8080)
+	Datastore = require('nedb'),
+	_tweets = new Datastore({
+		filename: './tweets.db',
+		autoload: true
+	}),
+	_users = new Datastore({
+		filename: './users.db',
+		autoload: true
+	});
+
 
 io.sockets.on('connection', function (socket) {
 	socket.on('register', function (data) {
-  		if (data.login && data.password && !_users[data.login]) {
-  			_users[data.login] = data;
-  			socket.emit('register', true);
-	  	} else {
-  			socket.emit('register', false);
-  		}
+		_users.findOne({login: data.login}, function(err, user) {
+	  		if (!user) {
+	  			_users.insert(data);
+	  			socket.emit('register', true);
+		  	} else {
+	  			socket.emit('register', false);
+	  		}
+		});
 	});
 
 	socket.on('login', function (data) {
-		if (_users[data.login] && _users[data.login].password === data.password) {
-			socket.user = data.login;
-			socket.emit('login', _users[data.login]);
-		} else {
-			socket.emit('login', false);
-		}
+		_users.findOne({login: data.login, password: data.password}, function(err, user) {
+			if (user) {
+				socket.user = data.login;
+				socket.emit('login', user);
+			} else {
+				socket.emit('login', false);
+			}
+		});
 	});
 
 	socket.on('tweet', function (data) {
 		if (socket.user) {
-			data.user = JSON.parse(JSON.stringify(_users[socket.user]));
-			delete data.password;
-			socket.broadcast.emit('tweet', data);
-			_tweets.push(data);
+			_users.findOne({login: socket.user}, function(err, user) {
+				delete user.password;
+				data.user = user;
+				socket.broadcast.emit('tweet', data);
+				_tweets.insert(data);
+			});
 		}
 	});
 
 	socket.on('load', function () {
-		socket.emit('load', _tweets);
+		_tweets.find({}, function(err, tweets) {
+			socket.emit('load', tweets);
+		});
 	});
 });
