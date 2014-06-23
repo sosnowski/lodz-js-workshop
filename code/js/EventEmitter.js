fe400cf50ded2c466b80bfda8ceceae7435949fd
Mateusz <mateusz@e-creativo.pl>
2014-06-23 23:10:47 +0200

    Small fixes

--- code/js/EventEmitter.js
+++ code/js/EventEmitter.js
@@ -11,7 +11,7 @@ App.EventEmitter.prototype.on = function (event, fn, scope) {
 
 App.EventEmitter.prototype.off = function (event, fn, scope) {
     if (typeof this._events[event] !== 'undefined') {
-        var n = _events[event].length;
+        var n = this._events[event].length;
         while (n--) {
             if (this._events[event][n].fn === fn && (typeof scope === 'undefined' || scope === this._events[event][n].scope)) {
                 this._events[event].splice(n, 1);
